1c23905e078528de7ee76637c15f4abebedab656
xuchuang <15004581109@163.com>
2024-07-29 12:15:22 +0800

    batch 尝试，通道数记录可变

--- Recon.py
+++ Recon.py
@@ -4,12 +4,10 @@
 '''
 import numpy as np
 import tables
-import mcmc
 import Detector
 from config import *
-from DetectorConfig import E0
+from DetectorConfig import chnums
 from tqdm import tqdm
-import LH
 from fsmp_reader import FSMPreader
 
 class DataType(tables.IsDescription):
@@ -23,97 +21,196 @@ class DataType(tables.IsDescription):
     z = tables.Float16Col(pos=4)        # z position
     E = tables.Float16Col(pos=5)        # Energy
     t = tables.Float16Col(pos=6)        # time
-    NPE = tables.Int16Col(pos=7)        # NPE
+    NPE = tables.Int32Col(pos=7)        # NPE
     Likelihood = tables.Float32Col(pos=8)
-    acceptz = tables.Int16Col(pos=9)
-    acceptr = tables.Int16Col(pos=10)
-    acceptE = tables.Int16Col(pos=11)
-    acceptt = tables.Int16Col(pos=12)
+    acceptz = tables.Int32Col(pos=9)
+    acceptr = tables.Int32Col(pos=10)
+    acceptt = tables.Int32Col(pos=11)
 
-def reconstruction(fsmp, sparsify, Entries, output, probe, pmt_pos, darkrate, timecalib, MC_step, record_mode):
+dtype = np.dtype([('EventID', '<i8'), ('step', '<i4'), ('x', '<f2'), ('y', '<f2'),
+                  ('z', '<f2'), ('E', '<f2'), ('t', '<f2'), ('NPE', '<i4'),
+                  ('Likelihood', '<f4'), ('acceptz', '<i4'), ('acceptr', '<i4'),
+                  ('acceptt', '<i4')])
+
+def LH(vertex, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib):
+    '''
+    计算 Likelihood
+    '''
+    PEt = zs + offsets[:,:,None] - vertex[:, 4][:,None,None] + timecalib[chs][:,:,None]
+    R = probe.genR(vertex, PEt, chs, fires)
+    index = np.arange(R.shape[2])[None, None, :] < s0s[:, :, None]
+    L1 = np.sum(np.log(R + darkrate[chs][:,:,None]) * index, axis=(1,2))
+    Rsum = probe.genRsum(vertex, chs, fires)
+    index = np.arange(Rsum.shape[1])[None, :] < fires[:, None]
+    L2 = - np.sum(Rsum * index, axis=1)
+    return L1 + L2
+
+# TODO
+def LHch():
+    pass
+
+# TODO
+def Regression(vertex, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib):
+    pass
+
+def concat(iterator, Entries):
+    '''
+    将多个事例数组拼接
+    eids.shape = (N, 1)
+    fires.shape = (N, 1)
+    chs.shape = (N, x)
+    offsets.shape = (N, x)
+    zs.shape = (N, x, y)
+    s0s.shape = (N, x)
+    nu_lcs.shape = (N, x)
+    '''
+    eids = np.zeros(Entries, dtype=np.int64)
+    fires = np.zeros(Entries, dtype=np.int32)
+    chs = np.zeros((Entries, chnums), dtype=np.int32)
+    offsets = np.zeros((Entries, chnums), dtype=np.float32)
+    zs = np.zeros((Entries, chnums, 10), dtype=np.float32)
+    s0s = np.zeros((Entries, chnums), dtype=np.int32)
+    nu_lcs = np.zeros((Entries, chnums), dtype=np.float32)
+    samplers = []
+    for iter, (eid, ch, offset, z, s0, nu_lc, _, sampler) in enumerate(iterator):
+        i = iter % Entries
+        # 取数后重置
+        if i == 0:
+            eids = np.zeros(Entries, dtype=np.int64)
+            fires = np.zeros(Entries, dtype=np.int32)
+            chs = np.zeros((Entries, chnums), dtype=np.int32)
+            offsets = np.zeros((Entries, chnums), dtype=np.float32)
+            zs = np.zeros((Entries, chnums, 10), dtype=np.float32)
+            s0s = np.zeros((Entries, chnums), dtype=np.int32)
+            nu_lcs = np.zeros((Entries, chnums), dtype=np.float32)
+            samplers = []
+        # 数据类型更正
+        s0 = s0.astype('int32')
+        eids[i] = eid
+        fires[i] = len(ch)
+        # 通道数量扩展
+        ch_increment = fires[i] - chs.shape[1]
+        if ch_increment > 0:
+            supply = np.zeros((Entries, ch_increment), dtype=np.int32)
+            chs = np.concatenate((chs, supply), axis=1)
+            s0s = np.concatenate((s0s, supply), axis=1)
+            supply = np.zeros((Entries, ch_increment), dtype=np.float32)
+            offsets = np.concatenate((offsets, supply), axis=1)
+            nu_lcs = np.concatenate((nu_lcs, supply), axis=1)
+            supply = np.zeros((Entries, ch_increment, zs.shape[2]), dtype=np.float32)
+            zs = np.concatenate((zs, supply), axis=1)
+        chs[i, :fires[i]] = ch
+        offsets[i, :fires[i]] = offset
+        s0s[i, :fires[i]] = s0
+        nu_lcs[i, :fires[i]] = nu_lc
+        # NPE 扩展
+        if z.shape[1] > zs.shape[2]:
+            supply = np.zeros((Entries, zs.shape[1], z.shape[1] - zs.shape[2]), dtype=np.float32)
+            zs = np.concatenate((zs, supply), axis=2)
+        zs[i, :z.shape[0], :z.shape[1]] = z
+        samplers.append(sampler)
+        # 取数
+        if i == Entries - 1:
+            yield eids, fires, chs, offsets, zs, s0s, nu_lcs, samplers
+    # 不足 Entries 个事例取完
+    yield eids[:i], fires[:i], chs[:i], offsets[:i], zs[:i], s0s[:i], nu_lcs[:i], samplers[:i]
+                    
+def resampleZ(iterators, events, maxs):
+    '''
+    返回 z 的重采样结果
+    ich.shape = (N, 1)
+    z_extend.shape = (N, 1, y)
+    s0.shape = (N, 1)
+    nu_lc.shape = (N, 1)
+    '''
+    ich = np.zeros((events), dtype=np.int32)
+    z_extend = np.zeros((events, maxs), dtype=np.float32)
+    s0 = np.zeros((events), dtype=np.int32)
+    nu_lc = np.zeros((events), dtype=np.float32)
+    for i, iterator in enumerate(iterators):
+        ich[i], z, s0[i], nu_lc[i] = next(iterator)
+        # NPE 扩展
+        if s0[i] > z_extend.shape[1]:
+            supply = np.zeros((events, s0[i] - z_extend.shape[1]), dtype=np.float32)
+            z_extend = np.concatenate((z_extend, supply), axis=1)
+        z_extend[i, :s0[i]] = z[:s0[i]]
+    return ich, z_extend, s0, nu_lc
+
+def Reconstruction(fsmp, sparsify, Entries, output, probe, pmt_pos, darkrate, timecalib, MC_step):
     '''
     reconstruction
     '''
-    # Create the output file, the group, tables
-    h5file = tables.open_file(output, mode="w", title="OSIRISDetector", filters = tables.Filters(complevel=9))
+    # create tables
+    h5file = tables.open_file(output, mode="w", title="Detector", filters = tables.Filters(complevel=9))
     group = "/"
-    InitTable = h5file.create_table(group, "Init", DataType, "Init")
-    init = InitTable.row
     ReconTable = h5file.create_table(group, "Recon", DataType, "Recon")
-    recon = ReconTable.row
-    if record_mode == "ON":
-        SampleTable = h5file.create_table(group, "Sample", DataType, "Sample")
-        sample = SampleTable.row
-
-    # start reconstruction
-    eid_start = 0
-    for eid, chs, offsets, zs, s0s, nu_lcs, _, sampler in tqdm(FSMPreader(sparsify, fsmp).rand_iter(MC_step)):
-        # 数据格式修正：将 s0s 转换为 int16
-        s0s = s0s.astype('int16')
-
+    for eids, fires, chs, offsets, zs, s0s, nu_lcs, samplers in tqdm(concat(FSMPreader(sparsify, fsmp).rand_iter(MC_step), Entries)):
         # 设定随机数
-        np.random.seed(eid % 1000000)
-        u = np.random.uniform(0, 1, (MC_step, variables))
-    
+        np.random.seed(eids[0] % 1000000) # 取第一个事例编号设定随机数种子
+        u_gibbs = np.random.uniform(0, 1, (MC_step, len(eids), gibbs_variables))
+        u_V = np.random.normal(0, 1, (MC_step, len(eids), V_variables))
+        
+        # test
+        Likelihood_vertex0 = LH(np.zeros((len(eids), 5)), fires, chs, offsets, zs, s0s, probe, darkrate, timecalib)
+
         # 给出 vertex, LogLikelihhood 的初值并记录
-        vertex0 = Detector.Init(zs, s0s, offsets, chs, pmt_pos, timecalib)
-        Likelihood_vertex0 = LH.LogLikelihood(vertex0, zs, s0s, offsets, chs, probe, darkrate, timecalib)
-        init['x'], init['y'], init['z'], init['E'], init['t'] = vertex0
-        init['EventID'] = eid
-        init.append()
+        vertex0 = Detector.Init(zs, s0s, offsets, chs, pmt_pos, timecalib) 
+        Likelihood_vertex0 = LH(vertex0, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib)
 
-        for recon_step in range(MC_step):
-            recon['EventID'] = eid
-            recon['step'] = recon_step
+        # gibbs iteration
+        for step in range(MC_step):
+            # 对 z 采样
+            ich_s, z_s, s0_s, nu_lc_s = resampleZ(samplers, len(eids), zs.shape[2])
+            ch_s = chs[np.arange(chs.shape[0]), ich_s]
+            z_o = zs[np.arange(zs.shape[0]), ich_s]
+            s0_o = s0s[np.arange(s0s.shape[0]), ich_s]
+            nu_lc_o = nu_lcs[np.arange(nu_lcs.shape[0]), ich_s]
+            offset_s = offsets[np.arange(offsets.shape[0]), ich_s]
+            LH_z_sample = LHch(vertex0, ch_s, z_s, s0_s, offset_s, probe, darkrate, timecalib)
+            LH_z_origin = LHch(vertex0, ch_s, z_o, s0_o, offset_s, probe, darkrate, timecalib)
+            accept_z = (nu_lc_o - nu_lc_s + LH_z_sample - LH_z_origin) > u_gibbs[step, :, 0]
+            # update z
+            s0s[np.arange(s0s.shape[0])[accept_z], ich_s[accept_z]] = s0_s[accept_z]
+            nu_lcs[np.arange(nu_lcs.shape[0])[accept_z], ich_s[accept_z]] = nu_lc_s[accept_z]
+            if z_s.shape[2] > z_o.shape[2]:
+                supply = np.zeros((zs.shape[0], zs.shape[1], z_s.shape[2] - z_o.shape[2]), dtype=np.float32)
+                zs = np.concatenate((zs, supply), axis=2)
+            zs[np.arange(zs.shape[0])[accept_z], ich_s[accept_z]] = z_s[accept_z]
+            # update Likelihood
+            Likelihood_vertex0[accept_z] = LH(vertex0, fires[accept_z], chs[accept_z], offsets[accept_z], zs[accept_z], s0s[accept_z], probe, darkrate, timecalib)
 
-            # 对 z 采样: 以 count 为权重，从所有 channel 随机采样的组合
-            expect = probe.callPE(vertex0)
-            i_ch, z, s0, nu_lc = next(sampler)
-            s0 = np.int16(s0) ## s0 的数据格式修正
-            T_i = probe.callT(chs[i_ch])
-            ratio_sample = np.sum(np.log(LH.callRt(z[:s0] + offsets[i_ch] + timecalib[chs[i_ch]], T_i + vertex0[-1]) * expect[chs[i_ch]] * vertex0[3] / E0 + darkrate[chs[i_ch]]))
-            ratio_origin = np.sum(np.log(LH.callRt(zs[i_ch][:s0s[i_ch]] + offsets[i_ch] + timecalib[chs[i_ch]], T_i + vertex0[-1]) * expect[chs[i_ch]] * vertex0[3] / E0 + darkrate[chs[i_ch]]))
-            criterion = nu_lcs[i_ch] - nu_lc + ratio_sample - ratio_origin
-            if criterion > np.log(u[recon_step, 0]):
-                s0s[i_ch] = s0
-                nu_lcs[i_ch] = nu_lc
-                zs[i_ch] = z
-                vertex0[3] = LH.glm(vertex0, zs, s0s, offsets, chs, probe, darkrate)[0] * E0 ## GLM 计算 E
-                Likelihood_vertex0 = LH.LogLikelihood(vertex0, zs, s0s, offsets, chs, probe, darkrate, timecalib)
-                recon['acceptz'] = 1
-            recon['NPE'] = np.sum(s0s)
+            # 对 r 采样
+            vertex1 = vertex0.copy()
+            vertex1[:, :3] = vertex1[:, :3] + r_sigma * u_V[step, :, :3]
+            if Detector.Boundary(vertex1):
+                # regression on E
+                vertex1[:, 3] = Regression(vertex1, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib)
+                Likelihood_vertex1 = LH(vertex1, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib)
+                accept_r = Likelihood_vertex1 - Likelihood_vertex0 > u_gibbs[step, :, 1]
+                # update r
+                vertex0[accept_r] = vertex1[accept_r]
+                Likelihood_vertex0[accept_r] = Likelihood_vertex1[accept_r]
 
-            # 对 V 采样: 球内随机晃动
-            vertex1 = mcmc.Perturb_posT(vertex0, u[recon_step, 1:5], r_max)
-            if Detector.Boundary(vertex1): ## 边界检查   
-                vertex1[3] = LH.glm(vertex1, zs, s0s, offsets, chs, probe, darkrate)[0] * E0 ## GLM 计算 E 
-                Likelihood_vertex1 = LH.LogLikelihood(vertex1, zs, s0s, offsets, chs, probe, darkrate, timecalib)
-                if record_mode == "ON": ## 记录采样结果
-                    sample['EventID'] = eid
-                    sample['step'] = recon_step
-                    sample['x'], sample['y'], sample['z'], sample['E'], sample['t'] = vertex1
-                    sample['Likelihood'] = Likelihood_vertex1
-                    sample.append()
-                if ((Likelihood_vertex1 - Likelihood_vertex0) > np.log(u[recon_step, 5])):
-                    vertex0 = vertex1
-                    Likelihood_vertex0 = Likelihood_vertex1
-                    recon['acceptr'] = 1
+            # 对 t 采样
+            vertex2 = vertex0.copy()
+            vertex2[:, 4] = vertex2[:, 4] + T_sigma * u_V[step, :, 3]
+            Likelihood_vertex2 = LH(vertex2, fires, chs, offsets, zs, s0s, probe, darkrate, timecalib)
+            accept_t = Likelihood_vertex2 - Likelihood_vertex0 > u_gibbs[step, :, 2]
+            # update t
+            vertex0[accept_t] = vertex2[accept_t]
+            Likelihood_vertex0[accept_t] = Likelihood_vertex2[accept_t]
 
-            # 记录重建数据
-            recon['x'], recon['y'], recon['z'], recon['E'], recon['t'] = vertex0
-            recon['Likelihood'] = Likelihood_vertex0
-            recon.append()
-
-        # 重建事例数：如果 Entries 不为 0，只重建 Entries 个事例
-        eid_start += 1
-        if Entries != 0:
-            if eid_start > Entries - 1:
-                break
-
-    # Flush into the output file
-    InitTable.flush() ## 初值
-    ReconTable.flush() ## 重建结果
-    if record_mode == "ON":
-        SampleTable.flush() ## 采样结果
+            # write into tables
+            result_step = np.hstack((eids, np.repeat(step, len(eids)), vertex0, np.sum(s0s, axis=1), Likelihood_vertex0, accept_z, accept_r, accept_t)).astype(dtype)
+            ReconTable.append(result_step)
+    
+    ReconTable.flush()
     h5file.close()
+
+
+
+
+
+
+
 
