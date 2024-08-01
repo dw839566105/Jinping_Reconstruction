952c866dcd0733d809b0638d542a8b50a4eb813f
xuchuang <15004581109@163.com>
2024-08-01 16:21:57 +0800

    移除 njit, numpy 转 cupy

--- Detector.py
+++ Detector.py
@@ -4,8 +4,7 @@
 Detector: jinping_1ton
 '''
 from DetectorConfig import *
-from numba import njit
-import numpy as np
+import cupy as cp
 import h5py
 
 def Boundary(vertex):
@@ -13,11 +12,10 @@ def Boundary(vertex):
     判断晃动是否超出边界
     探测器相关: Jinping_1ton
     '''
-    return np.sum(np.square(vertex[:, :3]), axis=1) <= 1
+    return cp.sum(cp.square(vertex[:, :3]), axis=1) <= 1
 
-@njit
 def legval(x, n):
-    res = np.zeros((n,) + x.shape)
+    res = cp.zeros((n,) + x.shape)
     res[0] = 1
     res[1] = x
     for i in range(2, n):
@@ -44,12 +42,12 @@ class Probe:
         '''
         # boundary
         v = vertex[:, :3]
-        rho = np.linalg.norm(v, axis=1)
-        rho = np.clip(rho, 0, 1)
+        rho = cp.linalg.norm(v, axis=1)
+        rho = cp.clip(rho, 0, 1)
         # calculate cos theta
-        cos_theta = np.cos(np.arctan2(np.linalg.norm(np.cross(v[:, None, :], self.pmt_pos), axis=-1), np.dot(v, self.pmt_pos.T)))
-        base_t = legval(cos_theta, np.max([self.coeff_pe.shape[0], self.coeff_time.shape[0]]))
-        base_r = legval(rho, np.max([self.coeff_pe.shape[1], self.coeff_time.shape[1]]))
+        cos_theta = cp.cos(cp.arctan2(cp.linalg.norm(cp.cross(v[:, None, :], self.pmt_pos), axis=-1), cp.dot(v, self.pmt_pos.T)))
+        base_t = legval(cos_theta, cp.max([self.coeff_pe.shape[0], self.coeff_time.shape[0]]))
+        base_r = legval(rho, cp.max([self.coeff_pe.shape[1], self.coeff_time.shape[1]]))
         return base_t, base_r
 
     def genR(self, vertex, PEt, sum_mode = True):
@@ -61,24 +59,24 @@ class Probe:
         '''
         base_t, base_r = self.genBase(vertex)
         # 计算空间项
-        NPE = np.exp(np.sum(np.matmul(base_t[:self.coeff_pe.shape[0]].T, self.coeff_pe) * base_r[:self.coeff_pe.shape[1]].T[None, :, :], axis=2))
+        NPE = cp.exp(cp.sum(cp.matmul(base_t[:self.coeff_pe.shape[0]].T, self.coeff_pe) * base_r[:self.coeff_pe.shape[1]].T[None, :, :], axis=2))
         # 计算 R
-        Ti = np.sum(np.matmul(base_t[:self.coeff_time.shape[0]].T, self.coeff_time) * base_r[:self.coeff_time.shape[1]].T[None, :, :], axis=2)
+        Ti = cp.sum(cp.matmul(base_t[:self.coeff_time.shape[0]].T, self.coeff_time) * base_r[:self.coeff_time.shape[1]].T[None, :, :], axis=2)
         t = PEt - Ti.T[:,:, None] - vertex[:, -1][:, None, None]
-        R = tau * (1 - tau) / ts * np.exp(- np.where(t < 0, t * (tau - 1), t * tau) / ts) * NPE.T[:, :, None] / E0
+        R = tau * (1 - tau) / ts * cp.exp(- cp.where(t < 0, t * (tau - 1), t * tau) / ts) * NPE.T[:, :, None] / E0
         if not sum_mode:
             # 返回单位能量 R
             return R
         R *= vertex[:, 3][:, None, None]
         # 分类计算 R 的积分
         down = - Ti.T - vertex[:, -1][:, None]
-        Rsum = np.zeros_like(down)
+        Rsum = cp.zeros_like(down)
         index1 = down > 0
-        Rsum[index1] = (tau - 1) * (np.exp(- tau * (down[index1] + wavel) / ts) - np.exp(- tau * down[index1] / ts))
+        Rsum[index1] = (tau - 1) * (cp.exp(- tau * (down[index1] + wavel) / ts) - cp.exp(- tau * down[index1] / ts))
         index2 = down < - wavel
-        Rsum[index2] = tau * (np.exp((1- tau) * (down[index2] + wavel) / ts) - np.exp((1- tau) * down[index2] / ts))
+        Rsum[index2] = tau * (cp.exp((1- tau) * (down[index2] + wavel) / ts) - cp.exp((1- tau) * down[index2] / ts))
         index3 = ~(index1 | index2)
-        Rsum[index3] = 1 + (tau - 1) * np.exp(- tau * (down[index3] + wavel) / ts) - tau * np.exp((1- tau) * down[index3] / ts)
+        Rsum[index3] = 1 + (tau - 1) * cp.exp(- tau * (down[index3] + wavel) / ts) - tau * cp.exp((1- tau) * down[index3] / ts)
         return R, Rsum * NPE.T * vertex[:, 3][:, None] / E0
 
     def genBasech(self, vertex, chs):
@@ -87,13 +85,13 @@ class Probe:
         '''
         # boundary
         v = vertex[:, :3]
-        rho = np.linalg.norm(v, axis=1)
-        rho = np.clip(rho, 0, 1)
+        rho = cp.linalg.norm(v, axis=1)
+        rho = cp.clip(rho, 0, 1)
         # calculate cos theta
         p = self.pmt_pos[chs]
-        cos_theta = np.cos(np.arctan2(np.linalg.norm(np.cross(v, p), axis=-1), np.sum(v * p, axis=1)))
-        base_t = legval(cos_theta, np.max([self.coeff_pe.shape[0], self.coeff_time.shape[0]]))
-        base_r = legval(rho, np.max([self.coeff_pe.shape[1], self.coeff_time.shape[1]]))
+        cos_theta = cp.cos(cp.arctan2(cp.linalg.norm(cp.cross(v, p), axis=-1), cp.sum(v * p, axis=1)))
+        base_t = legval(cos_theta, cp.max([self.coeff_pe.shape[0], self.coeff_time.shape[0]]))
+        base_r = legval(rho, cp.max([self.coeff_pe.shape[1], self.coeff_time.shape[1]]))
         return base_t, base_r    
 
     def genRch(self, vertex, PEt, chs):
@@ -104,37 +102,37 @@ class Probe:
         '''
         base_t, base_r = self.genBasech(vertex, chs)
         # 计算空间项
-        NPE = np.exp(np.sum((base_t[:self.coeff_pe.shape[0]].T @ self.coeff_pe) * base_r[:self.coeff_pe.shape[1]].T, axis=1))
+        NPE = cp.exp(cp.sum((base_t[:self.coeff_pe.shape[0]].T @ self.coeff_pe) * base_r[:self.coeff_pe.shape[1]].T, axis=1))
         # 计算 R
-        Ti = np.sum((base_t[:self.coeff_time.shape[0]].T @ self.coeff_time) * base_r[:self.coeff_time.shape[1]].T, axis=1)
+        Ti = cp.sum((base_t[:self.coeff_time.shape[0]].T @ self.coeff_time) * base_r[:self.coeff_time.shape[1]].T, axis=1)
         t = PEt - Ti.T[:, None] - vertex[:, -1][:, None]
-        R = tau * (1 - tau) / ts * np.exp(- np.where(t < 0, t * (tau - 1), t * tau) / ts) * NPE.T[:, None] * vertex[:, 3][:, None] / E0
+        R = tau * (1 - tau) / ts * cp.exp(- cp.where(t < 0, t * (tau - 1), t * tau) / ts) * NPE.T[:, None] * vertex[:, 3][:, None] / E0
         # 分类计算 R 的积分
         down = - Ti - vertex[:, -1]
-        Rsum = np.zeros_like(down)
+        Rsum = cp.zeros_like(down)
         index1 = down > 0
-        Rsum[index1] = (tau - 1) * (np.exp(- tau * (down[index1] + wavel) / ts) - np.exp(- tau * down[index1] / ts))
+        Rsum[index1] = (tau - 1) * (cp.exp(- tau * (down[index1] + wavel) / ts) - cp.exp(- tau * down[index1] / ts))
         index2 = down < - wavel
-        Rsum[index2] = tau * (np.exp((1- tau) * (down[index2] + wavel) / ts) - np.exp((1- tau) * down[index2] / ts))
+        Rsum[index2] = tau * (cp.exp((1- tau) * (down[index2] + wavel) / ts) - cp.exp((1- tau) * down[index2] / ts))
         index3 = ~(index1 | index2)
-        Rsum[index3] = 1 + (tau - 1) * np.exp(- tau * (down[index3] + wavel) / ts) - tau * np.exp((1- tau) * down[index3] / ts)
+        Rsum[index3] = 1 + (tau - 1) * cp.exp(- tau * (down[index3] + wavel) / ts) - tau * cp.exp((1- tau) * down[index3] / ts)
         return R, Rsum * NPE * vertex[:, 3] / E0
 
 def quantile_regression(arr):
-    return np.quantile(arr[arr != 0], tau)
+    return cp.quantile(arr[arr != 0], tau)
 
 def Init(zs, s0s, offsets, pmt_pos, timecalib):
     '''
     计算初值
     '''
-    vertex = np.zeros((s0s.shape[0], 5))
-    s0sum = np.sum(s0s, axis=1)
+    vertex = cp.zeros((s0s.shape[0], 5))
+    s0sum = cp.sum(s0s, axis=1)
     vertex[:, 3] = s0sum/ npe
     vertex[:, :3] = 1.5 * s0s @ pmt_pos / s0sum[:, None]
     PEt = zs.copy()
-    index = np.arange(zs.shape[2])[None, None, :] < s0s[:, :, None]
-    PEt = np.where(index, zs + offsets[:, :, None] + timecalib[None, :, None], 0)
-    vertex[:, -1] = np.apply_along_axis(quantile_regression, axis=1, arr=PEt.reshape(PEt.shape[0], -1))
+    index = cp.arange(zs.shape[2])[None, None, :] < s0s[:, :, None]
+    PEt = cp.where(index, zs + offsets[:, :, None] + timecalib[None, :, None], 0)
+    vertex[:, -1] = cp.apply_along_axis(quantile_regression, axis=1, arr=PEt.reshape(PEt.shape[0], -1))
     return vertex
 
 def LoadProbe(PEFile, TimeFile, PmtPos):
--- Recon.py
+++ Recon.py
@@ -2,8 +2,7 @@
 '''
 重建模拟数据的位置、能量
 '''
-import numpy as np
-from numba import njit
+import cupy as cp
 import h5py
 import Detector
 from config import *
@@ -16,7 +15,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # 重建数据储存类型
-dtype = np.dtype([('EventID', '<i8'), ('step', '<i4'), ('x', '<f2'), ('y', '<f2'),
+dtype = cp.dtype([('EventID', '<i8'), ('step', '<i4'), ('x', '<f2'), ('y', '<f2'),
                   ('z', '<f2'), ('E', '<f2'), ('t', '<f2'), ('NPE', '<i4'),
                   ('Likelihood', '<f4'), ('acceptz', '<i4'), ('acceptr', '<i4'),
                   ('acceptE', '<i4'), ('acceptt', '<i4')])
@@ -27,10 +26,10 @@ def LH(vertex, chs, offsets, zs, s0s, probe, darkrate, timecalib):
     '''
     PEt = zs + offsets[:, :, None] + timecalib[None, :, None]
     R, Rsum = probe.genR(vertex, PEt)
-    index = np.arange(R.shape[2])[None, None, :] < s0s[:, :, None]
-    L1 = np.sum(np.log(R + darkrate[chs][:, :, None]) * index, axis=(1,2))
+    index = cp.arange(R.shape[2])[None, None, :] < s0s[:, :, None]
+    L1 = cp.sum(cp.log(R + darkrate[chs][:, :, None]) * index, axis=(1,2))
     index = s0s > 0
-    L2 = - np.sum(Rsum * index, axis=1)
+    L2 = - cp.sum(Rsum * index, axis=1)
     return L1 + L2
 
 def LHch(vertex, chs, zs, s0s, offsets, probe, darkrate, timecalib):
@@ -39,8 +38,8 @@ def LHch(vertex, chs, zs, s0s, offsets, probe, darkrate, timecalib):
     '''
     PEt = zs + offsets[:, None] + timecalib[chs][:, None]
     R, Rsum = probe.genRch(vertex, PEt, chs)
-    index = np.arange(R.shape[1])[None, :] < s0s[:, None]
-    L1 = np.sum(np.log(R + darkrate[chs][:, None]) * index, axis=1)
+    index = cp.arange(R.shape[1])[None, :] < s0s[:, None]
+    L1 = cp.sum(cp.log(R + darkrate[chs][:, None]) * index, axis=1)
     return L1 + Rsum
 
 def glm(data):
@@ -48,29 +47,28 @@ def glm(data):
     result = sm.GLM(data[length: 2 * length], data[:length], family=sm.families.Poisson(link=sm.families.links.identity()), offset=data[2 * length:]).fit().params[0]
     return result
 
-@njit
 def histogram(data, weights, bins):
-    result = np.zeros((data.shape[0], data.shape[1], bins.shape[0] - 1))
+    result = cp.zeros((data.shape[0], data.shape[1], bins.shape[0] - 1))
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
-            result[i, j], _ = np.histogram(data[i, j][weights[i,j]], bins=bins)
+            result[i, j], _ = cp.histogram(data[i, j][weights[i,j]], bins=bins)
     return result
 
 def Regression(vertex, offsets, zs, s0s, probe, darkrate, timecalib):
     '''
     回归能量
     '''
-    index = np.arange(zs.shape[2])[None, None, :] < s0s[:, :, None]
+    index = cp.arange(zs.shape[2])[None, None, :] < s0s[:, :, None]
     PEt = zs + offsets[:, :, None] + timecalib[None, :, None]
-    bound = np.arange(0, wavel + tbin, tbin)
-    tx = np.arange(tbin / 2, wavel + tbin / 2, tbin)
-    tx = np.broadcast_to(tx, (zs.shape[0], zs.shape[1], tx.shape[0]))
+    bound = cp.arange(0, wavel + tbin, tbin)
+    tx = cp.arange(tbin / 2, wavel + tbin / 2, tbin)
+    tx = cp.broadcast_to(tx, (zs.shape[0], zs.shape[1], tx.shape[0]))
     X = probe.genR(vertex, tx, False) * tbin
     Y = histogram(PEt, index, bound)
-    darkrate_3d = np.expand_dims(darkrate, axis=(0, 2))
-    B = np.broadcast_to(darkrate_3d, (X.shape)) * tbin
-    data = np.column_stack((X.reshape(X.shape[0], -1), Y.reshape(Y.shape[0], -1), B.reshape(B.shape[0], -1)))
-    E = np.apply_along_axis(glm, 1, data)
+    darkrate_3d = cp.expand_dims(darkrate, axis=(0, 2))
+    B = cp.broadcast_to(darkrate_3d, (X.shape)) * tbin
+    data = cp.column_stack((X.reshape(X.shape[0], -1), Y.reshape(Y.shape[0], -1), B.reshape(B.shape[0], -1)))
+    E = cp.apply_along_axis(glm, 1, data)
     return E
 
 def concat(iterator, Entries):
@@ -83,23 +81,23 @@ def concat(iterator, Entries):
     s0s.shape = (N, chnums)
     nu_lcs.shape = (N, chnums)
     '''
-    eids = np.zeros(Entries, dtype=np.int64)
-    chs = np.zeros((Entries, chnums), dtype=np.int32)
-    offsets = np.zeros((Entries, chnums), dtype=np.float32)
-    zs = np.zeros((Entries, chnums, 10), dtype=np.float32)
-    s0s = np.zeros((Entries, chnums), dtype=np.int32)
-    nu_lcs = np.zeros((Entries, chnums), dtype=np.float32)
+    eids = cp.zeros(Entries, dtype=cp.int64)
+    chs = cp.zeros((Entries, chnums), dtype=cp.int32)
+    offsets = cp.zeros((Entries, chnums), dtype=cp.float32)
+    zs = cp.zeros((Entries, chnums, 10), dtype=cp.float32)
+    s0s = cp.zeros((Entries, chnums), dtype=cp.int32)
+    nu_lcs = cp.zeros((Entries, chnums), dtype=cp.float32)
     samplers = []
     for iter, (eid, ch, offset, z, s0, nu_lc, _, sampler) in enumerate(iterator):
         i = iter % Entries
         # 取数后重置
         if i == 0:
-            eids = np.zeros(Entries, dtype=np.int64)
-            chs = np.zeros((Entries, chnums), dtype=np.int32)
-            offsets = np.zeros((Entries, chnums), dtype=np.float32)
-            zs = np.zeros((Entries, chnums, 10), dtype=np.float32)
-            s0s = np.zeros((Entries, chnums), dtype=np.int32)
-            nu_lcs = np.zeros((Entries, chnums), dtype=np.float32)
+            eids = cp.zeros(Entries, dtype=cp.int64)
+            chs = cp.zeros((Entries, chnums), dtype=cp.int32)
+            offsets = cp.zeros((Entries, chnums), dtype=cp.float32)
+            zs = cp.zeros((Entries, chnums, 10), dtype=cp.float32)
+            s0s = cp.zeros((Entries, chnums), dtype=cp.int32)
+            nu_lcs = cp.zeros((Entries, chnums), dtype=cp.float32)
             samplers = []
         # 数据类型更正
         s0 = s0.astype('int32')
@@ -110,8 +108,8 @@ def concat(iterator, Entries):
         nu_lcs[i, ch] = nu_lc
         # NPE 扩展
         if z.shape[1] > zs.shape[2]:
-            supply = np.zeros((Entries, zs.shape[1], z.shape[1] - zs.shape[2]), dtype=np.float32)
-            zs = np.concatenate((zs, supply), axis=2)
+            supply = cp.zeros((Entries, zs.shape[1], z.shape[1] - zs.shape[2]), dtype=cp.float32)
+            zs = cp.concatenate((zs, supply), axis=2)
         zs[i, ch, :z.shape[1]] = z
         samplers.append(sampler)
         # 取数
@@ -128,16 +126,16 @@ def resampleZ(iterators, events, maxs):
     s0.shape = (N, 1)
     nu_lc.shape = (N, 1)
     '''
-    ich = np.zeros((events), dtype=np.int32)
-    z_extend = np.zeros((events, maxs), dtype=np.float32)
-    s0 = np.zeros((events), dtype=np.int32)
-    nu_lc = np.zeros((events), dtype=np.float32)
+    ich = cp.zeros((events), dtype=cp.int32)
+    z_extend = cp.zeros((events, maxs), dtype=cp.float32)
+    s0 = cp.zeros((events), dtype=cp.int32)
+    nu_lc = cp.zeros((events), dtype=cp.float32)
     for i, iterator in enumerate(iterators):
         ich[i], z, s0[i], nu_lc[i] = next(iterator)
         # NPE 扩展
         if s0[i] > z_extend.shape[1]:
-            supply = np.zeros((events, s0[i] - z_extend.shape[1]), dtype=np.float32)
-            z_extend = np.concatenate((z_extend, supply), axis=1)
+            supply = cp.zeros((events, s0[i] - z_extend.shape[1]), dtype=cp.float32)
+            z_extend = cp.concatenate((z_extend, supply), axis=1)
         z_extend[i, :s0[i]] = z[:s0[i]]
     return ich, z_extend, s0, nu_lc
 
@@ -152,12 +150,12 @@ def Reconstruction(fsmp, sparsify, Entries, output, probe, pmt_pos, darkrate, ti
 
         for eids, chs, offsets, zs, s0s, nu_lcs, samplers in tqdm(concat(FSMPreader(sparsify, fsmp).rand_iter(MC_step), Entries)):
             # 预分配储存数组
-            recon_step = np.zeros(len(eids), dtype=dtype)
+            recon_step = cp.zeros(len(eids), dtype=dtype)
 
             # 设定随机数
-            np.random.seed(eids[0] % 1000000) # 取第一个事例编号设定随机数种子
-            u_gibbs = np.log(np.random.uniform(0, 1, (MC_step, len(eids), gibbs_variables)))
-            u_V = np.random.normal(0, 1, (MC_step, len(eids), V_variables))
+            cp.random.seed(eids[0] % 1000000) # 取第一个事例编号设定随机数种子
+            u_gibbs = cp.log(cp.random.uniform(0, 1, (MC_step, len(eids), gibbs_variables)))
+            u_V = cp.random.normal(0, 1, (MC_step, len(eids), V_variables))
 
             # 给出 vertex, LogLikelihhood 的初值并记录
             vertex0 = Detector.Init(zs, s0s, offsets, pmt_pos, timecalib)
@@ -167,21 +165,21 @@ def Reconstruction(fsmp, sparsify, Entries, output, probe, pmt_pos, darkrate, ti
             for step in range(MC_step):
                 # 对 z 采样
                 ich_s, z_s, s0_s, nu_lc_s = resampleZ(samplers, len(eids), zs.shape[2])
-                ch_s = chs[np.arange(chs.shape[0]), ich_s]
-                z_o = zs[np.arange(zs.shape[0]), ch_s]
-                s0_o = s0s[np.arange(s0s.shape[0]), ch_s]
-                nu_lc_o = nu_lcs[np.arange(nu_lcs.shape[0]), ch_s]
-                offset_s = offsets[np.arange(offsets.shape[0]), ch_s]
+                ch_s = chs[cp.arange(chs.shape[0]), ich_s]
+                z_o = zs[cp.arange(zs.shape[0]), ch_s]
+                s0_o = s0s[cp.arange(s0s.shape[0]), ch_s]
+                nu_lc_o = nu_lcs[cp.arange(nu_lcs.shape[0]), ch_s]
+                offset_s = offsets[cp.arange(offsets.shape[0]), ch_s]
                 LH_z_sample = LHch(vertex0, ch_s, z_s, s0_s, offset_s, probe, darkrate, timecalib)
                 LH_z_origin = LHch(vertex0, ch_s, z_o, s0_o, offset_s, probe, darkrate, timecalib)
                 accept_z = (nu_lc_o - nu_lc_s + LH_z_sample - LH_z_origin) > u_gibbs[step, :, 0]
                 # update z
-                s0s[np.arange(s0s.shape[0])[accept_z], ch_s[accept_z]] = s0_s[accept_z]
-                nu_lcs[np.arange(nu_lcs.shape[0])[accept_z], ch_s[accept_z]] = nu_lc_s[accept_z]
+                s0s[cp.arange(s0s.shape[0])[accept_z], ch_s[accept_z]] = s0_s[accept_z]
+                nu_lcs[cp.arange(nu_lcs.shape[0])[accept_z], ch_s[accept_z]] = nu_lc_s[accept_z]
                 if z_s.shape[1] > z_o.shape[1]:
-                    supply = np.zeros((zs.shape[0], zs.shape[1], z_s.shape[1] - z_o.shape[1]), dtype=np.float32)
-                    zs = np.concatenate((zs, supply), axis=2)
-                zs[np.arange(zs.shape[0])[accept_z], ch_s[accept_z]] = z_s[accept_z]
+                    supply = cp.zeros((zs.shape[0], zs.shape[1], z_s.shape[1] - z_o.shape[1]), dtype=cp.float32)
+                    zs = cp.concatenate((zs, supply), axis=2)
+                zs[cp.arange(zs.shape[0])[accept_z], ch_s[accept_z]] = z_s[accept_z]
                 # update Likelihood
                 Likelihood_vertex0[accept_z] = LH(vertex0[accept_z], chs[accept_z], offsets[accept_z], zs[accept_z], s0s[accept_z], probe, darkrate, timecalib)
 
@@ -222,7 +220,7 @@ def Reconstruction(fsmp, sparsify, Entries, output, probe, pmt_pos, darkrate, ti
                 recon_step['z'] = vertex0[:, 2]
                 recon_step['E'] = vertex0[:, 3]
                 recon_step['t'] = vertex0[:, 4]
-                recon_step['NPE'] = np.sum(s0s, axis=1)
+                recon_step['NPE'] = cp.sum(s0s, axis=1)
                 recon_step['Likelihood'] = Likelihood_vertex0
                 recon_step['acceptz'] = accept_z
                 recon_step['acceptr'] = accept_r
